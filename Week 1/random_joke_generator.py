ac0b211c9424c781a8f80640c6985cd086e6c7c8
Amirul Faqih <fakiiahmad001@gmail.com>
2025-03-13 14:56:11 +0300

    Add random joke generator implementation in random_joke_generator.py

---
+++ Week 1/random_joke_generator.py
@@ -0,0 +1,12 @@
+import random
+
+jokes = [
+    "Why do Python programmers prefer dark mode? Because light attracts bugs! 🐛",
+    "Why did the function break up with the loop? It felt trapped in an endless cycle! 🔄",
+    "Why do Java developers wear glasses? Because they don’t C#! 🤓",
+    "Debugging: Being the detective in a crime movie where YOU are the murderer! 🔍",
+    "A SQL query walks into a bar, walks up to two tables and asks: ‘Can I join you?’ 🍻"
+]
+
+print("\n🤣 Random Joke Generator 🤣")
+print(random.choice(jokes))
